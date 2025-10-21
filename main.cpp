e55cb4e36a283e462771110d9b764327ab27c8a1
marwaaa.wawaa <wawaaamarwa@gmail.com>
2025-10-21 09:48:01 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    cout << "Selamat datang di GitHub!" << endl;
-    cout << "Ini program C++ pertama saya." << endl;
+using namespace std;
+struct nilai{
+    float Mtk;
+    float BIn;
+    float Big;
+    float IPA;
+};
+struct siswa{
+    string nama;
+    string NISN;
+    string jurusan;
+    nilai Nilai; // struct in struct
+};
+void tambahDataSiswa();//menambahkan data pada file siswa.txt
+void tampilDataSiswa();//membaca data dari file siswa.txt dan ditampilkan
+void cariSiswa();// berdasarkan NISN
+float nilaiAkhir();// nilai akhir = 40%matematika+30%IPA+20%B indonesia +20%B Inggris
+void ranking(); // menampilkan data siswa berdasarkan peringkatnya
+
+int main()
+{
+    cout << "Hello world!" << endl;
     return 0;
 }
